aef30c0b34e88311bcc46bb64f6e9a7983a67954
Trisha Gee <trishagee@github.com>
2014-02-21 11:56:04 +0100

    Page gets your location

--- src/main/resources/html/js/coffee.js
+++ src/main/resources/html/js/coffee.js
@@ -9,33 +9,32 @@ coffeeApp.controller('CustomerController', function ($scope) {
 });
 
 coffeeApp.factory('CoffeeShopLocator', function ($resource) {
-//    return $resource('http://localhost:8080/service/coffeeshop/nearest/51.4994678/-0.128888/', {}, {
-////        show: { method: 'GET' }
-//    });
-
-//    return $resource('http://localhost:8080/service/coffeeshop/dummy/', {}, {
-//                get: { method: 'JSONP', params: { callback: 'JSON_CALLBACK'}}
-//    });
-
     return $resource('http://localhost:8080/service/coffeeshop/nearest/:latitude/:longitude'
-        ,{latitude:'@latitude', longitude:'@longitude'}
-//        ,{}
-        ,{}
+        , {latitude: '@latitude', longitude: '@longitude'}
+        , {}
     )
 });
 
-coffeeApp.controller('CoffeeShopController', function ($scope, CoffeeShopLocator) {
+coffeeApp.controller('CoffeeShopController', function ($scope, $window, CoffeeShopLocator) {
 //    $scope.nearestCoffeeShop = CoffeeShopLocator.get({latitude: 51.4994678, longitude: -0.128888});
 //    $scope.nearestCoffeeShop = CoffeeShopLocator.get();
-    $scope.getNearest = function() {
-//        $scope.nearestCoffeeShop = CoffeeShopLocator.get();
-    $scope.nearestCoffeeShop = CoffeeShopLocator.get({latitude: 51.4994678, longitude: -0.128888});
+    $scope.getNearest = function () {
+        $scope.nearestCoffeeShop = CoffeeShopLocator.get({latitude: 51.4994678, longitude: -0.128888});
     };
 //    $scope.nearestCoffeeShop = 'My Shop';
+//});
+//
+//coffeeApp.controller('GeoLocation', function ($scope, $window) {
+    $scope.supportsGeo = $window.navigator;
+//    $scope.position = null;
+    $scope.getGeoLocation = function () {
+        window.navigator.geolocation.getCurrentPosition(function (position) {
+            $scope.$apply(function () {
+                $scope.position = position;
+            });
+        }, function (error) {
+            alert(error);
+        });
+    };
 });
-
-
-//    http://localhost:8080/coffeeshop/nearest/51.4994678/-0.128888
-//    http://localhost:8080/coffeeshop/nearest/51.4994678/-0.128888
-
 
