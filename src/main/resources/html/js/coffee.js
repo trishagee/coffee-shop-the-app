6a80103e05bb0659580fd846edbe1f58a798ae0b
Trisha Gee <trishagee@github.com>
2014-02-24 19:35:37 +0100

    Sends the coffee order to the back end (there is no back end, but hey...)

--- src/main/resources/html/js/coffee.js
+++ src/main/resources/html/js/coffee.js
@@ -6,7 +6,25 @@ coffeeApp.factory('CoffeeShopLocator', function ($resource) {
     )
 });
 
-coffeeApp.controller('CoffeeShopController', function ($scope, $window, CoffeeShopLocator) {
+coffeeApp.factory('CoffeeOrder', function ($resource) {
+    return $resource('http://localhost:8080/service/coffeeshop/:id/order/',
+        {id: '@coffeeShopId'}, {}
+    );
+});
+
+coffeeApp.service('LocalCoffeeShop', function () {
+    var localCoffeeShop;
+    return {
+        setShop: function (shop) {
+            localCoffeeShop = shop;
+        },
+        getShop: function () {
+            return localCoffeeShop;
+        }
+    };
+});
+
+coffeeApp.controller('CoffeeShopController', function ($scope, $window, CoffeeShopLocator, LocalCoffeeShop) {
     $scope.supportsGeo = $window.navigator;
     $scope.getNearestCoffeeShop = function (latitude, longitude) {
         $scope.nearestCoffeeShop = CoffeeShopLocator.get({latitude: latitude, longitude: longitude});
@@ -14,6 +32,7 @@ coffeeApp.controller('CoffeeShopController', function ($scope, $window, CoffeeSh
             //default coffee shop
             $scope.nearestCoffeeShop = CoffeeShopLocator.get({latitude: 51.4994678, longitude: -0.128888});
         }
+        LocalCoffeeShop.setShop($scope.nearestCoffeeShop);
     };
     $scope.getGeoLocation = function () {
         window.navigator.geolocation.getCurrentPosition(function (position) {
@@ -24,7 +43,7 @@ coffeeApp.controller('CoffeeShopController', function ($scope, $window, CoffeeSh
     };
 });
 
-coffeeApp.controller('DrinksController', function ($scope, $filter) {
+coffeeApp.controller('DrinksController', function ($scope, $filter, CoffeeOrder, LocalCoffeeShop) {
     //this could come from the coffee shop itself
     $scope.types = [
         {name: 'Americano', family: 'Coffee'},
@@ -33,25 +52,41 @@ coffeeApp.controller('DrinksController', function ($scope, $filter) {
         {name: 'Tea', family: 'That Other Drink'}
     ]
     $scope.sizes = ['Small', 'Medium', 'Large']
-    $scope.availableOptions = [{name: 'Soy', appliesTo:'milk'} , {name:'Skimmed', appliesTo:'milk'}, {name:'Caramel', appliesTo:'syrup'},
-        {name:'Decaf', appliesTo:'caffeine'}, {name:'Whipped Cream', appliesTo:'extras'}, {name: 'vanilla', appliesTo:'syrup'},
-        {name:'hazelnut', appliesTo:'syrup'}, {name:'sugar free', appliesTo:'syrup'}, {name: 'non fat', appliesTo:'milk'},
-        {name:'half fat', appliesTo:'milk'}, {name: 'half and half', appliesTo:'milk'}, {name:'half caf', appliesTo:'caffeine'},
-        {name:'chocolate powder', appliesTo:'extras'}, {name: 'double shot', appliesTo:'preparation'},
-        {name:'wet', appliesTo:'preparation'}, {name:'dry', appliesTo:'preparation'}, {name: 'organic', appliesTo:'milk'},
-        {name:'extra hot', appliesTo:'preparation'}]
+    $scope.availableOptions = [
+        {name: 'Soy', appliesTo: 'milk'} ,
+        {name: 'Skimmed', appliesTo: 'milk'},
+        {name: 'Caramel', appliesTo: 'syrup'},
+        {name: 'Decaf', appliesTo: 'caffeine'},
+        {name: 'Whipped Cream', appliesTo: 'extras'},
+        {name: 'vanilla', appliesTo: 'syrup'},
+        {name: 'hazelnut', appliesTo: 'syrup'},
+        {name: 'sugar free', appliesTo: 'syrup'},
+        {name: 'non fat', appliesTo: 'milk'},
+        {name: 'half fat', appliesTo: 'milk'},
+        {name: 'half and half', appliesTo: 'milk'},
+        {name: 'half caf', appliesTo: 'caffeine'},
+        {name: 'chocolate powder', appliesTo: 'extras'},
+        {name: 'double shot', appliesTo: 'preparation'},
+        {name: 'wet', appliesTo: 'preparation'},
+        {name: 'dry', appliesTo: 'preparation'},
+        {name: 'organic', appliesTo: 'milk'},
+        {name: 'extra hot', appliesTo: 'preparation'}
+    ]
 
     //TODO: remove
     $scope.drink = {};
     $scope.drink.selectedOptions = [];
 
-    $scope.addOption = function() {
+    $scope.addOption = function () {
         if (!$scope.drink.selectedOptions) {
             $scope.drink.selectedOptions = [];
         }
         $scope.drink.selectedOptions.push($filter('lowercase')($scope.newOption));
         $scope.newOption = '';
     };
+    $scope.giveMeCoffee = function () {
+        CoffeeOrder.save({ id: LocalCoffeeShop.getShop().allValues.openStreetMapId}, $scope.drink);
+    }
 
 });
 
